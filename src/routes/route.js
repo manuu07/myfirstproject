591a55ac297c47042feb63b93b379f38a39b31d6
manuu07 <manusrivastava229@gmail.com>
2022-10-20 03:28:38 +0530

    Consecutive Numbers Assignment Code

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,69 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
-
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+const express=require('express');
+const router=express.Router()
 
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+// -write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7]
+// 4 is missing
 
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
+router.get('/sol1',function(req,res){
+    let arr=[1,2,3,5,6,7]
+    let sum=0                         //sum of the all the elements present in the array
+    let n=arr.length+1               //n is number of elements in the array
+    let total=(n*(n+1))/2            //total is the total of the elements(including the missing number)
+    for (let i = 0; i < arr.length; i++) {
+        sum+=arr[i]      //sum=sum+arr[i]
+    }
+    let missingNumber=total-sum
+    res.send({data:missingNumber})
 })
 
 
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
+//write an api which gives the missing number in an array of integers starting from anywhere….e.g 
+  // [33, 34, 35, 37, 38]: 36 is missing
+
+  router.get('/sol2',function(req,res){
+    let arr2=[33,34,35,37,38]
+    let sum=0                                                 //sum of the all the elements present in the array
+    let n=arr2.length+1                                      //n is number of elements in the array
+    let total=(n*(arr2[0]+arr2[arr2.length-1])/2)            //total of the elements(including the missing number)
+    for (let i = 0; i < arr2.length; i++) {
+        sum+=arr2[i]              //sum=sum+arr2[i]
+    }
+    let missingNumber=total-sum
+    res.send({data:missingNumber})
+  })
 
 
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
+module.exports=router
 
 
 
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
 
-module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
