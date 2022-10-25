fc606591a7fb9ac5362f1cf217dcee696f116974
manuu07 <manusrivastava229@gmail.com>
2022-10-25 12:19:37 +0530

    bookSchema

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,10 @@
-const express = require('express');
-const router = express.Router();
-const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
+const express=require('express')
+const router=express.Router()
+const bookModel=require('../models/bookModel.js')
+const bookController=require('../controllers/bookController')
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+router.post("/createBook", bookController.createBook)
+router.get("/getBooks",bookController.getBooks)
 
-router.post("/createUser", UserController.createUser  )
+module.exports=router;
 
-router.get("/getUsersData", UserController.getUsersData)
-
-module.exports = router;
