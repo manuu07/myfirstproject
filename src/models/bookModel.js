b6912217d9ad93141314633b76b762ddfed120ba
manuu07 <manusrivastava229@gmail.com>
2022-10-26 13:41:04 +0530

    mongodb assignment-2

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,31 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose=require('mongoose')
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    authorName: String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: {
-        indianPrice: String,
-        europePrice: String,
+const bookSchema=mongoose.Schema({
+    bookName:{
+        type:String,
+        required:true,
+        unique:true
     },
-    sales: {type: Number, default: 10}
-}, { timestamps: true });
+    authorName:String,
+    price:{
+        indianPrice:String,
+        europeanPrice:String
+    },
+    year:{
+        type:Number,
+        default:2022
+    },
+    tags:[String],
+    totalPages:{
+        type:Number,
+        default:100
+    },
+    stockAvailable:Boolean
 
+})
 
-module.exports = mongoose.model('Book', bookSchema) //users
-
-//Validation:
-//require:true
-//unique
-// default
-
-//String
-//Number
-//Date
-//Boolean
-// Arrays
-// Object
-// ObjectId
-// Buffer - not cover
+module.exports= mongoose.model('book2',bookSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+const express=require('express')
+const router=express.Router()
+const bookModel=require('../models/bookModel')
+const bookController=require('../controllers/bookController')
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
 
-router.post("/createUser", UserController.createUser  )
+router.post('/createBook', bookController.createBook)
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get('/bookList', bookController.bookList)
 
-router.post("/createBook", BookController.createBook  )
+router.post('/getBooksInYear',bookController.getBooksInYear)
 
-router.get("/getBooksData", BookController.getBooksData)
+router.post('/getParticularBooks',bookController.getParticularBooks)
 
-module.exports = router;
+router.get('/getINRBooks',bookController.getINRBooks)
+
+router.get('/getRandomBooks',bookController.getRandomBooks)
+
+module.exports= router;
